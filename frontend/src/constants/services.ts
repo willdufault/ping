3b5d686b8b7ba806d6af24e62cd8aefb6abc97a5
wduf <wduf02@gmail.com>
2026-07-25 11:46:34 -0400

    feat: add cloudfront to frontend

--- frontend/src/constants/services.ts
+++ frontend/src/constants/services.ts
@@ -3,14 +3,16 @@ import ec2Icon from "../assets/images/aws-ec2.webp"
 import lambdaIcon from "../assets/images/aws-lambda.webp"
 import s3Icon from "../assets/images/aws-s3.webp"
 import dynamodbIcon from "../assets/images/aws-dynamodb.webp"
+import cloudfrontIcon from "../assets/images/aws-cloudfront.webp"
 
-export const services = ["EC2", "Lambda", "S3", "DynamoDB"] as const
+export const services = ["EC2", "Lambda", "S3", "DynamoDB", "CloudFront"] as const
 
 export const serviceLabels: Record<Service, string> = {
   EC2: "EC2",
   Lambda: "Lambda",
   S3: "S3",
   DynamoDB: "DDB",
+  CloudFront: "CF",
 }
 
 export const serviceIcons: Record<Service, string> = {
@@ -18,4 +20,5 @@ export const serviceIcons: Record<Service, string> = {
   Lambda: lambdaIcon,
   S3: s3Icon,
   DynamoDB: dynamodbIcon,
+  CloudFront: cloudfrontIcon,
 }
