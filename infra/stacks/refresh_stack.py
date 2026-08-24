5497cc634627efbd015e83f80856b06d827c8556
wduf <wduf02@gmail.com>
2026-08-24 17:10:09 -0400

    infra: import ddb table into refresh stack

---
+++ infra/stacks/refresh_stack.py
@@ -0,0 +1,98 @@
+from pathlib import Path
+
+import aws_cdk as cdk
+from aws_cdk.aws_lambda_python_alpha import PythonFunction
+from constructs import Construct
+
+
+class RefreshStack(cdk.NestedStack):
+    def __init__(
+        self,
+        scope: Construct,
+        id: str,
+        database_table: cdk.aws_dynamodb.TableV2,
+        **kwargs
+    ) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        check_services_lambda_path = (
+            Path(__file__).parents[2] / "backend" / "lambdas" / "check_services"
+        )
+        check_services_lambda_log_group = cdk.aws_logs.LogGroup(
+            self,
+            "CheckServicesLambdaLogGroup",
+            retention=cdk.aws_logs.RetentionDays.TWO_WEEKS,
+        )
+
+        check_services_lambda_role = cdk.aws_iam.Role(
+            self,
+            "CheckServicesLambdaRole",
+            role_name="ping-check-services-lambda-role",
+            assumed_by=cdk.aws_iam.ServicePrincipal("lambda.amazonaws.com"),
+            inline_policies={
+                "RoleAccess": cdk.aws_iam.PolicyDocument(
+                    statements=[
+                        cdk.aws_iam.PolicyStatement(
+                            actions=["dynamodb:GetItem", "dynamodb:PutItem"],
+                            resources=[database_table.table_arn],
+                        ),
+                        cdk.aws_iam.PolicyStatement(
+                            actions=[
+                                "ec2:DescribeInstances",
+                                "s3:ListBuckets",
+                                "lambda:ListFunctions",
+                                "dynamodb:ListTables",
+                                "cloudfront:ListDistributions",
+                            ],
+                            resources=["*"],
+                        ),
+                    ]
+                )
+            },
+        )
+
+        check_services_lambda = PythonFunction(
+            self,
+            "CheckServicesLambda",
+            function_name="ping-check-services-lambda",
+            runtime=cdk.aws_lambda.Runtime.PYTHON_3_13,
+            timeout=cdk.Duration.seconds(30),
+            handler="main",
+            entry=str(check_services_lambda_path),
+            log_group=check_services_lambda_log_group,
+            role=check_services_lambda_role,
+        )
+
+        scheduler_role = cdk.aws_iam.Role(
+            self,
+            "SchedulerRole",
+            role_name="ping-refresh-scheduler-role",
+            assumed_by=cdk.aws_iam.ServicePrincipal(  # type:ignore
+                "scheduler.amazonaws.com"
+            ),
+            inline_policies={
+                "InvokeLambda": cdk.aws_iam.PolicyDocument(
+                    statements=[
+                        cdk.aws_iam.PolicyStatement(
+                            actions=["lambda:InvokeFunction"],
+                            resources=[check_services_lambda.function_arn],
+                        )
+                    ]
+                )
+            },
+        )
+
+        cdk.aws_scheduler.CfnSchedule(
+            self,
+            "CheckServicesSchedule",
+            name="ping-refresh-schedule",
+            schedule_expression="cron(0/30 * * * ? *)",
+            schedule_expression_timezone="America/New_York",
+            flexible_time_window=cdk.aws_scheduler.CfnSchedule.FlexibleTimeWindowProperty(
+                mode="OFF",
+            ),
+            target=cdk.aws_scheduler.CfnSchedule.TargetProperty(
+                arn=check_services_lambda.function_arn,
+                role_arn=scheduler_role.role_arn,
+            ),
+        )
--- infra/stacks/root_stack.py
+++ infra/stacks/root_stack.py
@@ -3,10 +3,16 @@ from constructs import Construct
 
 from stacks.api_stack import ApiStack
 from stacks.database_stack import DatabaseStack
+from stacks.refresh_stack import RefreshStack
 
 
 class RootStack(cdk.Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-        DatabaseStack(self, "ping-database", environment)
-        ApiStack(self, "ping-api", environment)
+        database_stack = DatabaseStack(self, "ping-database")
+        ApiStack(self, "ping-api")
+        RefreshStack(
+            self,
+            "ping-refresh",
+            database_table=database_stack.status_history_table,
+        )
